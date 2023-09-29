08552424bffba65e58ef9ea30e0bdab8cc20aba2
Tinuola Adeyemi <Tinucy@yahoo.com>
2023-09-29 16:38:52 +0100

    setting up and example

---
+++ chat-app/src/components/chat/Chat.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Home from "../../pages/Home";
+import "./Chat.scss";
+
+export default function Chat() {
+  return (
+    <div className="chat">
+      <div className="chat__header">Headerrrr</div>
+      <div className="chat__body">Body</div>
+    </div>
+  );
+}
---
+++ chat-app/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Chat from "../components/chat/Chat";
+
+export default function Home() {
+  return (
+    <div>
+      <Chat />
+    </div>
+  );
+}
